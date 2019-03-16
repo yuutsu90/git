d65df8840d10752fcb0541d4956a3c7161760180
yuutsu90 <yuutsu90@naver.com>
2019-03-17 08:35:23 +0900

    initial commit

--- 객체지향프로그래밍/Java/ObjectOriented2019-01/src/kr/ac/kookmin/cs/oop/ch1/hello.java
+++ 객체지향프로그래밍/Java/ObjectOriented2019-01/src/kr/ac/kookmin/cs/oop/ch1/hello.java
@@ -1,13 +1,25 @@
 package kr.ac.kookmin.cs.oop.ch1;
 
-public class hello {
+public class Hello {
+
+	public static int sum(int n, int m) {
+		return n + m;
+	}
 	
 
+	
+	
 	public static void main(String[] args) {
-		int n = 21;
-		System.out.println("내 나이는 " + n);
 		
-		// TODO Auto-generated method stub
+		int i = 20;
+		int s;
+		char a;
+		
+		s = sum(i,10);
+		a = '?';
+		System.out.println(a);
+		System.out.println("Hello");
+		System.out.println(s);
 
 	}
 
